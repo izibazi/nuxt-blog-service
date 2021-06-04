1251a2bb1762b921c16158693be0cbd0e4a7ebbc
izibazi <izibazi@gmail.com>
2021-06-04 15:27:21 +0900

    page/index.vueの作成

--- blog-app/pages/index.vue
+++ blog-app/pages/index.vue
@@ -1,65 +1,42 @@
-<template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">nuxt-blog-service</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+  section.container
+    el-card(style="flex: 1")
+      div.clearfix(slot="header")
+        span ログイン
+      form
+        div.form-content
+          span ユーザーID
+          el-input(placeholder="username" v-model="formData.id")
+        div.form-content
+          el-checkbox(v-model="isCreateMode") アカウントを作成する
+        div.text-right
+          el-button(type="primary") {{ buttonText }}
+
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  asyncData() {
+    return {
+      isCreateMode: false,
+      formData: {
+        id: '',
+      },
+    }
+  },
+
+  computed: {
+    buttonText() {
+      return this.$data.isCreateMode ? '作成' : '更新'
+    },
+  },
+})
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss">
+.form-content {
+  margin: 16px 0;
 }
 </style>
