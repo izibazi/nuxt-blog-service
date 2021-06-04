aa948e7bb58a9521d274097cd6d3978d06015f6a
izibazi <izibazi@gmail.com>
2021-06-04 12:50:24 +0900

    TheHeader.vueを追加

---
+++ blog-app/components/TheHeader.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  div
+    el-menu(mode="horizontal" :router="true")
+      el-menu-item(index="1" style="pointer-events:none") Nuxt Diary App
+      el-menu-item(index="2" :route="{path: '/posts/'}") 投稿
+      el-menu-item(index="3" :route="{path: '/login/'}" style="float: right") ログイン
+      el-menu-item(index="4" :route="{path: '/posts/new'}" style="float: right") 新規投稿
+</template>
--- blog-app/layouts/default.vue
+++ blog-app/layouts/default.vue
@@ -1,5 +1,15 @@
-<template>
-  <div>
-    <Nuxt />
-  </div>
+<template lang="pug">
+  div
+    TheHeader
+    Nuxt.container
 </template>
+
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+
+export default {
+  components: {
+    TheHeader,
+  },
+}
+</script>
